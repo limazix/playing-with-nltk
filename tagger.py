3452d9ee46e0540778771cea50ae37d78d78be18
Bruno Lima Cardoso <blima@br.ibm.com>
2016-11-28 21:43:32 -0200

    Checking if the taggers are trained and already exists as files at static folder

--- tagger.py
+++ tagger.py
@@ -1,24 +1,34 @@
 # encoding: utf-8
 
+import os
 import nltk
 from nltk.corpus import floresta, mac_morpho
 from nltk.tokenize import word_tokenize
 from pickle import dump, load
 
+
 class Tagger:
-    def if_exists(self, corpus_name, name): 
-        return False
+
+    def build_path(self, corpus_name, tagger_name):
+        return "./taggers/" + corpus_name + "_" + tagger_name + ".pkl"
+
+    def if_exists(self, corpus_name, tagger_name):
+        path = self.build_path(corpus_name, tagger_name)
+        return os.path.isfile(path)
 
     def save_tagger(self, tagger, corpus_name, tagger_name):
-        output = open("./taggers/" + corpus_name + "_" + tagger_name + ".pkl", "wb")
+        path = self.build_path(corpus_name, tagger_name)
+        output = open(path, "wb")
         dump(tagger, output, -1)
         output.close()
 
     def get_tagger(self, corpus_name, train_sents, tagger_name, tagger_class, backoff):
-        if(self.if_exists(corpus_name, tagger_name)):
-            input = open("./taggers/" + corpus_name + "_" + tagger_name + ".pkl", "rb")
+        if self.if_exists(corpus_name, tagger_name):
+            path = self.build_path(corpus_name, tagger_name)
+            input = open(path, "rb")
             tagger = load(input)
             input.close()
+            return tagger
         else:
             tagger = tagger_class(train_sents, backoff=backoff)
             self.save_tagger(tagger, corpus_name, tagger_name)
@@ -29,13 +39,16 @@ class Tagger:
         default_tagger = nltk.DefaultTagger('NN')
 
         print "Train Unigram"
-        unigram_tagger = self.get_tagger(corpus_name, train_sents, "unigram", nltk.UnigramTagger, default_tagger)
+        unigram_tagger = self.get_tagger(
+            corpus_name, train_sents, "unigram", nltk.UnigramTagger, default_tagger)
 
         print "Train Bigram"
-        bigram_tagger = self.get_tagger(corpus_name, train_sents, "bigram", nltk.BigramTagger, unigram_tagger)
+        bigram_tagger = self.get_tagger(
+            corpus_name, train_sents, "bigram", nltk.BigramTagger, unigram_tagger)
 
         print "Train Trigram"
-        trigram_tagger = self.get_tagger(corpus_name, train_sents, "trigram", nltk.TrigramTagger, bigram_tagger)
+        trigram_tagger = self.get_tagger(
+            corpus_name, train_sents, "trigram", nltk.TrigramTagger, bigram_tagger)
 
         return trigram_tagger
 
@@ -51,8 +64,9 @@ class Tagger:
         floresta_result = self.run_tagger("floresta", floresta, tokenized)
 
         print "\n###### Mac Morpho Corpus #####"
-        mac_morpho_result = self.run_tagger("mac_morpho", mac_morpho, tokenized)
+        mac_morpho_result = self.run_tagger(
+            "mac_morpho", mac_morpho, tokenized)
 
         result = {"floresta": floresta_result, "mac morpho": mac_morpho_result}
-        
+
         return result
